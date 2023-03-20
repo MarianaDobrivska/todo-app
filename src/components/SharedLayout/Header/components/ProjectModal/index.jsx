b58894fde7a62806f404c48990113be1d398166b
MarianaDobrivska <dobrivska.marry@gmail.com>
2023-03-20 18:48:56 +0200

    add ability to close modal by esc and backdrop

--- src/components/SharedLayout/Header/components/ProjectModal/index.jsx
+++ src/components/SharedLayout/Header/components/ProjectModal/index.jsx
@@ -1,44 +1,33 @@
 import s from "./Modal.module.css";
 import TodoStore from "../../../../../store/todo";
-import { useNotify } from "../../../../../hooks";
+import { ModalWrapper } from "../../../../shared/Modal";
 
 export const Modal = ({ close }) => {
-  const { Notification, setNotify } = useNotify();
-
   const handleSubmit = (e) => {
     e.preventDefault();
     if (e.target !== e.currentTarget) return;
     const formData = new FormData(e.target);
     const projectName = formData.get("project");
-    if (projectName.trim() === "") {
-      setNotify({ status: "Warning", text: "Please enter project name" });
-      return;
-    }
-
+    if (projectName.trim() === "") return;
     TodoStore.addProject(projectName);
     close();
     e.target.reset();
   };
 
   return (
-    <>
-      <div className={s.wrapper}>
-        <div className={s.modalBackground}>
-          <form onSubmit={handleSubmit} className={s.form}>
-            <h1 className={s.title}>Add Project</h1>
-            <input
-              className={s.input}
-              type="text"
-              name="project"
-              placeholder="Enter project name"
-            />
-            <button type="submit" className={s.button}>
-              Add
-            </button>
-          </form>
-        </div>
-      </div>
-      <Notification />
-    </>
+    <ModalWrapper close={close}>
+      <form onSubmit={handleSubmit} className={s.form}>
+        <h1 className={s.title}>Add Project</h1>
+        <input
+          className={s.input}
+          type="text"
+          name="project"
+          placeholder="Enter project name"
+        />
+        <button type="submit" className={s.button}>
+          Add
+        </button>
+      </form>
+    </ModalWrapper>
   );
 };
--- src/components/shared/Modal/index.jsx
+++ src/components/shared/Modal/index.jsx
@@ -1,19 +1,36 @@
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import s from "./Modal.module.css";
+import { BsFillXCircleFill } from "react-icons/bs";
 
 const modalRoot = document.querySelector("#modal-root");
 
 export const ModalWrapper = ({ children, close }) => {
+  const handleModalClose = (e) => {
+    if (e.target === e.currentTarget) {
+      close();
+    }
+  };
+
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.keyCode === 27) {
+        close();
+      }
+    }
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [close]);
+
   return createPortal(
-    <div className={s.wrapper}>
+    <div className={s.wrapper} onClick={(e) => handleModalClose(e)}>
       <div className={s.modalBackground}>
         {children}
-        <button
-          type="button"
-          className={s.button}
-          onClick={(e) => e.target === e.currentTarget && close()}
-        >
-          Close
+        <button type="button" className={s.button}>
+          <BsFillXCircleFill className={s.icon} onClick={close} />
         </button>
       </div>
     </div>,
